0e95594d73027b5318c75a619f946c6e31cb23f4
MohcineDev <mouhcinedev@gmail.com>
2023-11-05 22:12:54 +0100

    split ips and create textareas

--- src/views/IPs.vue
+++ src/views/IPs.vue
@@ -18,21 +18,23 @@
       <div>
         <label for="splitInto">Into&nbsp;:&nbsp;</label>
         <input
-        title="split into"
+          title="split into"
           type="number"
           :value="splitInto"
           id="splitInto"
           min="2"
           max="5"
-          @input="e=> splitInto = parseInt(e.target.value)"
+          @input="(e) => (splitInto = parseInt(e.target.value))"
         />
       </div>
       <button @click="splitIps" class="split">Split</button>
     </div>
     <div class="textareas">
       <template v-for="(n, index) in textAreasCount" :key="index">
-          <textarea name="" :id="`list${n}`"></textarea>
-          <span></span>
+  {{ console.log(n) }}
+  <textarea  :id="`list${n}`" :value="list[index].toString().replace(/,/g, '')"></textarea>
+  
+        <span></span>
       </template>
     </div>
   </section>
@@ -40,8 +42,7 @@
 
 <script setup>
 import { ref } from "vue";
-const textAreasCount = ref(0)
-
+const textAreasCount = ref(0);
 const targetIps = ref(`192.168.1.1
 192.168.1.0
 192.168.1.2
@@ -49,16 +50,13 @@ const targetIps = ref(`192.168.1.1
 192.168.1.10
 255.255.255.255
 172.16.0.1`);
+let list = ref([[],[],[],[],[]])
 
 const splitInto = ref(2);
 let textareas = "";
+ 
 
-let list1Textarea = document.querySelector("#list1");
-let list2Textarea = document.querySelector("#list2");
-let list3Textarea = document.querySelector("#list3");
-let list4Textarea = document.querySelector("#list4");
-let list5Textarea = document.querySelector("#list5");
-
+////update span value when target ips changes
 function targetIpsChange() {
   console.log(targetIps);
   document.querySelector(" #ips+span").textContent =
@@ -66,20 +64,12 @@ function targetIpsChange() {
 }
 
 let validIps = [];
-let list1 = "",
-  list2 = "",
-  list3 = "",
-  list4 = "",
-  list5 = "";
 
 function splitIps() {
   textareas = document.querySelector(".textareas");
-console.log(splitInto)
 
-///how many text areas to create 0 --> 5
-textAreasCount.value = splitInto.value
   const ips = targetIps.value;
-  (list1 = ""), (list2 = ""), (list3 = ""), (list4 = ""), (list5 = "");
+ 
   validIps = [];
 
   if (!targetIps.value) {
@@ -95,8 +85,14 @@ textAreasCount.value = splitInto.value
     ? (splitInto.value = 2)
     : (splitInto.value = splitInto.value);
 
+  ///place it here to make sure splitInto is between 2 & 5
+  ///how many text areas to create 0 --> 5
+  textAreasCount.value = splitInto.value;
+
+
+
   ///create textarea element
-//  createTextArea(splitInto.value);
+  //  createTextArea(splitInto.value);
 
   //remove empty lines
   ips.split("\n").forEach((item) => {
@@ -117,9 +113,10 @@ textAreasCount.value = splitInto.value
   if (parseInt(splitInto.value) === 5) {
     splitIntoFunc(5);
   }
+  /*
   document.querySelectorAll(".textareas span").forEach((elem) => {
     elem.textContent = elem.previousElementSibling.value.split("\n").length - 1;
-  });
+  });*/
 }
 
 function createTextArea(nbr) {
@@ -129,7 +126,7 @@ function createTextArea(nbr) {
         .querySelectorAll("textarea")
         .forEach((elem) => textareas.removeChild(elem))
     : null;
-
+  //remove span elements
   textareas.querySelectorAll(".textareas span").length >= 1
     ? textareas
         .querySelectorAll("span")
@@ -144,35 +141,37 @@ function createTextArea(nbr) {
     textareas.appendChild(textArea);
     textareas.appendChild(span);
   }
-
-  list1Textarea = document.querySelector("#list1");
-  list2Textarea = document.querySelector("#list2");
-  list3Textarea = document.querySelector("#list3");
-  list4Textarea = document.querySelector("#list4");
-  list5Textarea = document.querySelector("#list5");
-
-  textareas
-    .querySelectorAll("textarea")
-    .forEach((txtArea) =>
-      txtArea.addEventListener("click", (e) => e.target.select())
-    );
 }
+
 //////---------------
 function splitIntoFunc(splitNbr) {
+  console.log('textAreasCount : ', textAreasCount.value)
+  let list1=[]
+let list2=[]
+let list3=[]
+let list4=[]
+let list5=[]
   validIps.forEach((element, index) => {
     if (splitNbr === 2) {
-      if (index % 2 == 0) {
-        list1 += `${element}\n`;
-      } else list2 += `${element}\n`;
-    } else if (splitNbr === 3) {
+      if (index % 2 == 0) 
+      {
+        list1.push(element+"\n");
+      } 
+      else list2.push(element+"\n");
+    } 
+    else if (splitNbr === 3) {
       if (index % 3 == 0) {
-        list1 += `${element}\n`;
-      } else if (index % 2 == 0) {
-        list2 += `${element}\n`;
-      } else list3 += `${element}\n`;
-    } else if (splitNbr === 4) {
+        list1.push(element+"\n");
+
+      } 
+      else if (index % 2 == 0) {
+        list2.push(element+"\n");
+      } 
+      else list3.push(element+"\n");
+    } 
+    else if (splitNbr === 4) {
       if (index % 4 == 0) {
-        list1 += `${element}\n`;
+        list1.push(element+"\n");
       } else if (index % 3 == 0) {
         list2 += `${element}\n`;
       } else if (index % 2 == 0) {
@@ -190,18 +189,13 @@ function splitIntoFunc(splitNbr) {
       } else list5 += `${element}\n`;
     }
   });
-
-  list1Textarea ? (list1Textarea.value = list1) : null;
-  list2Textarea ? (list2Textarea.value = list2) : null;
-  list3Textarea ? (list3Textarea.value = list3) : null;
-  list4Textarea ? (list4Textarea.value = list4) : null;
-  list5Textarea ? (list5Textarea.value = list5) : null;
+ 
+  list.value = [list1, list2,list1, list2, list1]
 }
 //////---------------
 </script>
 
 <style scoped>
-
 .wrapper {
   width: 90%;
   max-width: 1200px;
