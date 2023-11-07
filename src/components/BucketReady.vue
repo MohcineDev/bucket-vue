cb3f6de1e1dfe7e512580bc584382df557d5e6a1
MohcineDev <mouhcinedev@gmail.com>
2023-11-07 23:47:02 +0100

    generate a ready bucket

---
+++ src/components/BucketReady.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="input_container">
+    <input type="text" name="" :value="bucketName" id="" />
+    <button @click="generate">generate</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let bucketName = ref("");
+let bucketLangth = 25;
+let alpha = "az12er15ty64u8i7o9p6qs3k2sdj21f5h4g8n9b7v5xcw";
+let arratofalpha = alpha.split("");
+
+function generate() {
+    let bucket = "";
+  for (let index = 0; index < bucketLangth; index++) {
+    let num = Math.floor(Math.random() * arratofalpha.length);
+    bucket += arratofalpha[num];
+  }
+
+  bucketName.value = bucket;
+}
+generate()
+</script>
+
+<style scoped>
+.input_container {
+  display: flex;
+  justify-content: center;
+  padding: 1rem 0;
+  height: 15vh;
+  align-items: end;
+}
+</style>
