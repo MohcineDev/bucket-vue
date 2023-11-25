bb5c32af61f006aab881c062723d270a52e88a23
MohcineDev <mouhcinedev@gmail.com>
2023-11-25 18:25:46 +0100

    create FitlerTextarea component

---
+++ src/components/FilterTextarea.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <textarea :id="ID" :value="myValue" :placeholder="placeHolder" @input="e=> $emit('handleChange', e)"></textarea>
+  </div>
+</template>
+
+<script setup>
+defineProps(["placeHolder", "ID", "myValue"]);
+</script>
+
+<style scoped>
+
+ textarea {
+  width: 300px;
+  height: 200px;
+}
+
+</style>
